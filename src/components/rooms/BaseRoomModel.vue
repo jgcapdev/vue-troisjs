6a17b66690be1942c860bc59d01e77fd3c14255f
jgcapdev <jorge.gomez-carrillo-external@capgemini.com>
2022-07-25 14:43:04 +0200

    feat: improving switching models and meshes

--- src/components/rooms/BaseRoomModel.vue
+++ src/components/rooms/BaseRoomModel.vue
@@ -1,47 +1,57 @@
 <template>
-  <GltfModel
-    @load="createMesh($event, 1, 'wall')"
-    src="/assets/models/roomgood.gltf"
-    :scale="scale"
-    :position="position"
-    :rotation="rotation"
-  />
-  <GltfModel
-    @load="createMesh($event, 1, 'floor')"
-    src="/assets/models/roomgood.gltf"
-    :scale="scale"
-    :position="position"
-    :rotation="rotation"
-  />
-  <GltfModel
-    @load="createMesh($event, 1, 'rug')"
-    src="/assets/models/roomgood.gltf"
-    :scale="scale"
-    :position="position"
-    :rotation="rotation"
-  />
-  <GltfModel
-    @load="createMesh($event, 1, 'couch001')"
-    src="/assets/models/roomgood.gltf"
-    :scale="scale"
-    :position="position"
-    :rotation="rotation"
-  />
-  <GltfModel
-    @load="createMesh($event, 1, 'couch003')"
-    src="/assets/models/roomgood.gltf"
-    :scale="scale"
-    :position="position"
-    :rotation="rotation"
-  />
+  <div v-if="showModel">
+    <GltfModel
+      @load="createMesh($event, 500, 'wall')"
+      src="/assets/models/roomgood.gltf"
+      :scale="scale"
+      :position="position"
+      :rotation="rotation"
+    />
+    <GltfModel
+      @load="createMesh($event, 500, 'floor')"
+      src="/assets/models/roomgood.gltf"
+      :scale="scale"
+      :position="position"
+      :rotation="rotation"
+    />
+    <GltfModel
+      @load="createMesh($event, 500, 'rug')"
+      src="/assets/models/roomgood.gltf"
+      :scale="scale"
+      :position="position"
+      :rotation="rotation"
+    />
+    <GltfModel
+      @load="createMesh($event, 500, 'couch001')"
+      src="/assets/models/roomgood.gltf"
+      :scale="scale"
+      :position="position"
+      :rotation="rotation"
+    />
+    <GltfModel
+      @load="createMesh($event, 500, 'couch003')"
+      src="/assets/models/roomgood.gltf"
+      :scale="scale"
+      :position="position"
+      :rotation="rotation"
+    />
+  </div>
+
+  <div v-else>
+    <Mesh ref="wall" @created="create" :scale="scale" :position="position" :rotation="rotation">
+      <BoxGeometry />
+      <BasicMaterial />
+    </Mesh>
+  </div>
 </template>
 
 <script>
-import { GltfModel, BasicMaterial, BufferGeometry } from 'troisjs';
+import { GltfModel, BasicMaterial, BufferGeometry, BoxGeometry } from 'troisjs';
 import { InstancedMesh, Object3D, DynamicDrawUsage } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export default {
-  components: { GltfModel, BasicMaterial, BufferGeometry },
+  components: { GltfModel, BasicMaterial, BufferGeometry, BoxGeometry },
   props: {
     position: {
       type: Object,
@@ -56,21 +66,27 @@ export default {
       default: { x: 0, y: 0, z: 0 },
     },
   },
+  data() {
+    return {
+      showModel: false,
+    };
+  },
   methods: {
     createMesh(gltf, count, groupname) {
+      console.log(gltf);
       for (let i = 0; i < gltf.scene.children.length; i++) {
         let geometry = gltf.scene.children[i].geometry.clone();
         let material = gltf.scene.children[i].material;
         let instance = new InstancedMesh(geometry, material, count);
 
-        instance.instanceMatrix.setUsage(DynamicDrawUsage);
+        //instance.instanceMatrix.setUsage(DynamicDrawUsage);
 
         const dummy = new Object3D();
 
         dummy.position.set(this.position.x, this.position.y, this.position.z);
         dummy.updateMatrix();
-        instance.setMatrixAt(i, dummy.matrix);
-        instance.instanceMatrix.needsUpdate = true;
+        //instance.setMatrixAt(i, dummy.matrix);
+        // instance.instanceMatrix.needsUpdate = true;
 
         if (groupname) {
           gltf.scene.children.forEach((e) => {
@@ -80,21 +96,26 @@ export default {
           gltf.scene.add(instance);
         }
       }
-
-      /*
+    },
+    create(e) {
       const loader = new GLTFLoader();
+      let mesh = this.$refs.wall.mesh;
 
       loader.load('/assets/models/roomgood.gltf', function (gltf) {
         let _wallMesh = gltf.scene.getObjectByName('wall');
-        const _floorMesh = gltf.scene.getObjectByName('floor').geometry;
+        const _floorMesh = gltf.scene.getObjectByName('floor');
         const _rugMesh = gltf.scene.getObjectByName('rug').geometry;
         const _couchOneMesh = gltf.scene.getObjectByName('couch001').geometry;
         const _couchTwoMesh = gltf.scene.getObjectByName('couch003').geometry;
 
-        let instanced = new InstancedMesh(_wallMesh.geometry, _wallMesh.material, 500);
-        instanced.name = 'wallAll';
+        let instancedWalls = new InstancedMesh(_wallMesh.geometry, _wallMesh.material, 500);
+        instancedWalls.name = 'walls';
+        instancedWalls.userData.mass = 1;
+
+        let instancedFloors = new InstancedMesh(_floorMesh.geometry, _floorMesh.material, 500);
+        instancedFloors.name = 'floors';
+        instancedFloors.userData.mass = 1;
       });
-      */
     },
   },
 };
